084f969d12ef7d413bcb77874021c932f4859982
Yan Oliveira <oliveira.yan02@gmail.com>
2018-11-22 18:28:19 -0200

    Basic server configuration with nodeJS

---
+++ README.md
@@ -0,0 +1,9 @@
+# basicNodeJS
+Basic and standard structure of a backend application using NodeJS.
+
+Features used:
+  - Express
+  - Nodemon
+  - EditorConfig
+  - esLint e Prettier
+  - nunJucks
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  passsword: 'docker',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
